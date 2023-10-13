73ba72e9998a25df26323b1051fdecca25068c65
Aalishan17Raza <aalishan69raza@gmail.com>
2023-10-13 22:22:10 +0530

    ...

--- script.js
+++ script.js
@@ -1,9 +1,21 @@
+let currentYear;
 function setCurrYear() {
     let currYear = new Date().getFullYear();
+    currentYear = currYear;
     document.getElementById("cal-title").textContent = currYear + " " + document.getElementById("cal-title").textContent;
 }
 
+
 function init() {
     setCurrYear();
+    document.body.addEventListener('click', (e) => {
+        if (e.target.tagName == 'I') {
+            let span = e.target.parentElement;
+            // span.style.backgroundColor = e.target.style.color;
+            // e.target.classList.add('i-selected');
+            console.log(e.target.style.color);
+            console.log(span);
+        }
+    });
 }
 init();
