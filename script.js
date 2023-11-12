4686bab300b7f9b366a6964ef3e1ecef088a42fb
Aalishan17Raza <aalishan69raza@gmail.com>
2023-11-12 22:31:43 +0530

    made a jan table

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
 let currentYear;
+let currentColor='var(--default-color)';
+function init() {
+    setCurrYear();
+    document.body.addEventListener('click', (e) => {
+        resetColor();
+        if (e.target.tagName == 'I') {
+            let I = e.target;
+            let span = e.target.parentElement;
+            currentColor=`var(--${I.id})`;
+            console.log(currentColor);
+            span.style.border = `thick solid var(--${I.id})`;
+            I.style.color = '#ffffff';
+            I.style.backgroundColor = `var(--${I.id})`;
+        }
+    });
+}
+init();
 function setCurrYear() {
     let currYear = new Date().getFullYear();
     currentYear = currYear;
@@ -7,24 +24,11 @@ function setCurrYear() {
 
 function resetColor() {
     let I = document.querySelectorAll('i');
+    currentColor='var(--default-color)';
+    console.log(currentColor);
     I.forEach(it=>{
         it.style.color = `var(--${it.id})`;
         it.style.backgroundColor= '#ffffff';
         it.parentElement.style.border = 'none';
     });
 }
-
-function init() {
-    setCurrYear();
-    document.body.addEventListener('click', (e) => {
-        resetColor();
-        if (e.target.tagName == 'I') {
-            let I = e.target;
-            let span = e.target.parentElement;
-            span.style.border = `thick solid var(--${I.id})`;
-            I.style.color = '#ffffff';
-            I.style.backgroundColor = `var(--${I.id})`;
-        }
-    });
-}
-init();
